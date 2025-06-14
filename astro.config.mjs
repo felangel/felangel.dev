984f3f55d5c2a033de1ae94a5869e1ab4a68c235
Felix Angelov <felangelov@gmail.com>
2025-06-14 17:29:32 -0500

    chore: add sitemap (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -8,4 +9,5 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	integrations: [sitemap()],
 });
